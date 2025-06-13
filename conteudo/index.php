000226e924bff75b200c91b6d8a98477e590e733
Weslley Feitosa <weslleybarbosa662@gmail.com>
2025-06-13 15:31:39 -0300

    Update index.php

--- conteudo/index.php
+++ conteudo/index.php
@@ -5,14 +5,37 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://cdn.boxicons.com/fonts/basic/boxicons.min.css' rel='stylesheet'>
+    <link href='https://cdn.boxicons.com/fonts/brands/boxicons-brands.min.css' rel='stylesheet'>
 </head>
 <body>
     <div class="box_login">
         <div class="parte1">
-            <img src="img/imagem_principal.png" alt="">
+            <div class="imagem">
+                <img src="img/imagem_principal.png" alt="">
+            </div>
+            <div class="link_criar_conta">
+                <a href="">Se não tiver conta se cadastre</a>
+            </div>
         </div>
         <div class="parte2">
+            <h1 class="titulo_login">Login</h1>
+            <div class="formulario">
+                <form action="" method="post">
+                    <div class="campo_gmail">
+                        <i class='bx  bx-envelope-open'></i> 
+                        <input type="email" name="email" id="email">
+                    </div>
+                    <div class="campo_senha">
+                        <i class='bx  bx-lock'></i>
+                        <input type="password" name="senha" id="senha">
+                        <i class='bx  bx-eye'  ></i> 
+                    </div>
 
+                    <input type="submit" value="Login" id="bnt_login">
+                </form>
+            </div>
         </div>
     </div>
 </body>
